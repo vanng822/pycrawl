d6a475a5b64e274a8596608b55b72647762c4d35
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-04-28 23:43:04 +0200

    test argument parse fail

--- tests/fib_test.py
+++ tests/fib_test.py
@@ -4,5 +4,12 @@ import fib
 
 class FibTest(unittest.TestCase):
     
+    def test_fib_str(self):
+        try:
+            fib.fib("10")
+            self.fail("Expected un error of TypeError due to requirement of integer in argument")
+        except TypeError as e:
+            pass
+        
     def test_fib(self):
         self.assertEqual(fib.fib(10), 55)
