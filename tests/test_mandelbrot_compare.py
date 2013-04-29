5a8ee0c006965a23732161eae59e2cd8bf44fb43
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2013-04-30 00:05:49 +0200

    Add some profiling

--- tests/test_mandelbrot_compare.py
+++ tests/test_mandelbrot_compare.py
@@ -3,36 +3,55 @@ import inspect
 import cmandelbrot
 from mandelbrot import iterator
 import time
+
+import cProfile
  
 class MandelbrotTest(unittest.TestCase):
     
     def test_p_c_time(self):
+        pr = cProfile.Profile()
+        pr.enable()
         t = time.time()
         iterator.Iterator()
-        ptotal =  time.time() - t
+        ptotal = time.time() - t
         print '\nMandelbrot Python Elapsed %.06f' % ptotal
+        pr.disable()
+        pr.print_stats()
         
+        pr = cProfile.Profile()
+        pr.enable()
         t = time.time()
         cmandelbrot.mandelbrot()
         ctotal = time.time() - t
         
         print '\nC Mandelbrot Python Elapsed %.06f' % ctotal
-        print '\n %f times slower' % (ptotal/ctotal)
+        
+        pr.disable()
+        pr.print_stats()
+        
+        print '\n %f times slower' % (ptotal / ctotal)
         
         self.assertTrue(ctotal < ptotal)
         
     def test_c_p_time(self):
-        
+        pr = cProfile.Profile()
+        pr.enable()
         t = time.time()
         cmandelbrot.mandelbrot()
         ctotal = time.time() - t
         print '\nC Mandelbrot Python Elapsed %.06f' % ctotal
+        pr.disable()
+        pr.print_stats()
         
+        
+        pr = cProfile.Profile()
+        pr.enable()
         t = time.time()
         iterator.Iterator()
-        ptotal =  time.time() - t
+        ptotal = time.time() - t
         print '\nMandelbrot Python Elapsed %.06f' % ptotal
-        
-        print '\n %f times slower' % (ptotal/ctotal)
+        pr.disable()
+        pr.print_stats()
+        print '\n %f times slower' % (ptotal / ctotal)
         
         self.assertTrue(ctotal < ptotal)
